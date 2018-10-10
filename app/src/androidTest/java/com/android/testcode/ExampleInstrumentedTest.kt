99e77f1ed7c3fe6f2bedf58f764cdc167329886d
saulocasian <saulo.raul.garcia.casian@hp.com>
2018-10-10 14:37:55 -0700

    Added Unit tests and Instrumentation tests

--- app/src/androidTest/java/com/android/testcode/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/android/testcode/ExampleInstrumentedTest.kt
@@ -7,6 +7,26 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.CoreMatchers.allOf
+import android.support.test.rule.ActivityTestRule
+import android.view.View
+import com.android.testcode.features.RegisterActivity
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import android.support.design.widget.TextInputLayout
+import org.hamcrest.Description
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.Espresso.onView
+import android.support.v7.widget.RecyclerView
+import android.support.annotation.IdRes
+
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +35,134 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule @JvmField
+    val mActivityRule = ActivityTestRule(RegisterActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.android.testcode", appContext.packageName)
+    fun testRegisterUser_Success() {
+
+        val emailAddress = "firstname.lastname@g.com"
+        val password = "Password1234"
+
+        //find the email address edit text and type in the email address
+        onView(withId(R.id.userName)).perform(typeText(emailAddress), closeSoftKeyboard())
+
+        //find the password edit text and type in the password
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard())
+
+        //click the signup button
+        onView(withId(R.id.register)).perform(click())
+
+        //check that we can see the success screen with success message
+        val successString = "Result"//InstrumentationRegistry.getTargetContext().getString("TestCode")
+        onView(withId(R.id.result)).check(matches(allOf(withText(successString), isDisplayed())))
     }
+
+    @Test
+    fun testRegisterUser_InvalidUserName() {
+
+        val emailAddress = "firstname.lag.com"
+        val password = "Password1234"
+
+        //find the email address edit text and type in the email address
+        onView(withId(R.id.userName)).perform(typeText(emailAddress), closeSoftKeyboard())
+
+        //find the password edit text and type in the password
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard())
+
+        //click the signup button
+        onView(withId(R.id.register)).perform(click())
+
+        //check that we can see the success screen with success message
+        val errorString = "Not a valid email address!"
+
+        onView(withId(R.id.userNameWrapper)).check(matches(hasTextInputLayoutErrorText(errorString)))
+    }
+
+    @Test
+    fun testRegisterUser_InvalidPasswordNonAlphaNumeric() {
+
+        val emailAddress = "firstname.l@ag.com"
+        val password = "Pass=word1234"
+
+        //find the email address edit text and type in the email address
+        onView(withId(R.id.userName)).perform(typeText(emailAddress), closeSoftKeyboard())
+
+        //find the password edit text and type in the password
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard())
+
+        //click the signup button
+        onView(withId(R.id.register)).perform(click())
+
+        //check that we can see the success screen with success message
+        val errorString = "Not a valid password!"
+
+        onView(withId(R.id.passwordWrapper)).check(matches(hasTextInputLayoutErrorText(errorString)))
+    }
+
+    @Test
+    fun testRegisterUser_InvalidPasswordLessThan5Chars() {
+
+        val emailAddress = "firstname.l@ag.com"
+        val password = "Pas"
+
+        //find the email address edit text and type in the email address
+        onView(withId(R.id.userName)).perform(typeText(emailAddress), closeSoftKeyboard())
+
+        //find the password edit text and type in the password
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard())
+
+        //click the signup button
+        onView(withId(R.id.register)).perform(click())
+
+        //check that we can see the success screen with success message
+        val errorString = "Not a valid password!"
+
+        onView(withId(R.id.passwordWrapper)).check(matches(hasTextInputLayoutErrorText(errorString)))
+    }
+
+    @Test
+    fun testRegisterUser_InvalidPasswordSequenceFollowedBySameSequence() {
+
+        val emailAddress = "firstname.l@ag.com"
+        val password = "1Pass1Pass"
+
+        //find the email address edit text and type in the email address
+        onView(withId(R.id.userName)).perform(typeText(emailAddress), closeSoftKeyboard())
+
+        //find the password edit text and type in the password
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard())
+
+        //click the signup button
+        onView(withId(R.id.register)).perform(click())
+
+        //check that we can see the success screen with success message
+        val errorString = "Not a valid password!"
+
+        onView(withId(R.id.passwordWrapper)).check(matches(hasTextInputLayoutErrorText(errorString)))
+    }
+
+
+    fun hasTextInputLayoutErrorText(expectedErrorText: String): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+
+
+            public override fun matchesSafely(view: View): Boolean {
+                if (view !is TextInputLayout) {
+                    return false
+                }
+
+                val error = view.error ?: return false
+
+                val hint = error.toString()
+
+                return expectedErrorText.equals(hint)
+            }
+
+            override fun describeTo(description: Description?) {
+
+            }
+        }
+    }
+
 }
--- app/src/test/java/com/android/testcode/ExampleUnitTest.kt
+++ app/src/test/java/com/android/testcode/ExampleUnitTest.kt
@@ -1,8 +1,26 @@
 package com.android.testcode
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.LiveData
+import com.android.testcode.data.dao.UserDataSource
+import com.android.testcode.data.model.User
+import com.android.testcode.features.ui.listusers.ListUsersViewModel
+import com.google.common.truth.Truth
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.mockito.InjectMocks
+import org.mockito.junit.MockitoJUnit
+import org.mockito.Mock
+import android.arch.lifecycle.ViewModel
+import com.android.testcode.features.ui.register.RegisterViewModel
+import org.mockito.MockitoAnnotations
+import org.junit.Before
+
+
+
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +28,98 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @get:Rule
+    val rxSchedulerRule = RxSchedulerRule()
+
+    @get:Rule
+    val mockitoRule = MockitoJUnit.rule()
+
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    lateinit var classUnderTest: RegisterViewModel
+
+    @Mock
+    private val mDataModel: UserDataSource? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        classUnderTest = RegisterViewModel(mDataModel)
     }
+
+    fun <T> LiveData<T>.testObserver() = TestObserver<T>().also {
+        observeForever(it)
+    }
+
+
+    @Test
+    fun `testValidUserRegistration`() {
+        val installationStatus = classUnderTest.responseLiveData.testObserver()
+
+        classUnderTest.registerUser("userName@domain", "password123")
+        //Note that this tests the real call to onFakeInstall(). It will take 3 seconds. It's possible to play with time in RX unit testing. See a next article maybe :P
+
+        Truth.assert_()
+                .that(installationStatus.observedValues.first()).isTrue()
+    }
+
+    @Test
+    fun `testInvalidUserName`() {
+        val installationStatus = classUnderTest.userNameErrorResponseLiveData.testObserver()
+
+        classUnderTest.registerUser("userNamedomain", "password123")
+        //Note that this tests the real call to onFakeInstall(). It will take 3 seconds. It's possible to play with time in RX unit testing. See a next article maybe :P
+
+        Truth.assert_()
+                .that(installationStatus.observedValues.first()).isTrue()
+    }
+
+    @Test
+    fun `testInvalidPasswordLessThan5Char`() {
+        val installationStatus = classUnderTest.passwordErrorResponseLiveData.testObserver()
+
+        classUnderTest.registerUser("userNamedomain@a.com", "pas")
+        //Note that this tests the real call to onFakeInstall(). It will take 3 seconds. It's possible to play with time in RX unit testing. See a next article maybe :P
+
+        Truth.assert_()
+                .that(installationStatus.observedValues.first()).isTrue()
+    }
+
+    @Test
+    fun `testInvalidPasswordLessMoreThan12`() {
+        val installationStatus = classUnderTest.passwordErrorResponseLiveData.testObserver()
+
+        classUnderTest.registerUser("userNamedomain@a.com", "qazwsxedcrfv5tgb6yhn7u")
+        //Note that this tests the real call to onFakeInstall(). It will take 3 seconds. It's possible to play with time in RX unit testing. See a next article maybe :P
+
+        Truth.assert_()
+                .that(installationStatus.observedValues.first()).isTrue()
+    }
+
+    @Test
+    fun `testInvalidPasswordNonAlphaNumeric`() {
+        val installationStatus = classUnderTest.passwordErrorResponseLiveData.testObserver()
+
+        classUnderTest.registerUser("userNamedomain@a.com", "qz2$")
+        //Note that this tests the real call to onFakeInstall(). It will take 3 seconds. It's possible to play with time in RX unit testing. See a next article maybe :P
+
+        Truth.assert_()
+                .that(installationStatus.observedValues.first()).isTrue()
+    }
+
+    @Test
+    fun `testInvalidPasswordSequenceFollowedbySameSequence`() {
+        val installationStatus = classUnderTest.passwordErrorResponseLiveData.testObserver()
+
+        classUnderTest.registerUser("userNamedomain@a.com", "1hello1hello")
+        //Note that this tests the real call to onFakeInstall(). It will take 3 seconds. It's possible to play with time in RX unit testing. See a next article maybe :P
+
+        Truth.assert_()
+                .that(installationStatus.observedValues.first()).isTrue()
+    }
+
+
 }
